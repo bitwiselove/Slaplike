b9c9d5ed5f32e5a6fa6acbada857d8a348ae16c3
Brandon Newton <bitwiselove@gmail.com>
2016-02-29 22:09:55 -0800

    Rip Mithril....
    While Mithril.js was fun to try I could sense writing a ton of
    boilerplate was upcoming to share state throughout the applications
    components. React and Redux to the rescue!

--- app/old.mithril.js
+++ app/old.mithril.js
@@ -6,57 +6,78 @@ var Player = {
   }
 };
 
-var Search = {
+var SearchForm = {
   term: m.prop(''),
   results: m.prop(false),
   search: function(e) {
     e.preventDefault();
-
-    m.request({method: 'GET', url: '/similar/' + Search.term()}).then(body => {
-      Search.results(body.similarartists.artist);
-    }).then(m.redraw);
+    m.route('/like/' + this.term());
   },
   view: function() {
     var results = this.results() ? this.results() : [];
     var player = this.results() ? m.component(Player) : null;
 
-    return m('div', [
+    return m('div.searchForm', [
       m('form', {onsubmit: this.search}, [
         m('label', 'Author'),
         m('input', {onchange: m.withAttr('value', this.term), value: this.term()}),
         m('button[type=button]', {onclick: this.search.bind(this)}, 'Search'),
-      ]),
-      m('div.playerContainer', [
-        player
-      ]),
-      m('div.resultsContainer', [
-        results.filter(resultsWithImages).map(result => {
-          return m.component(Result, {
-            name: result.name,
-            image: result.image[2]['#text'],
-            match: result.match
-          });
-        })
       ])
     ]);
   }
 };
 
+var SearchResults = {
+  controller: function() {
+    this.results = m.prop(false);
+
+    m.request({method: 'GET', url: '/similar/' + m.route.param('artist')}).then(body => {
+      this.results(body.similarartists.artist);
+    });
+  },
+  view: function(ctrl) {
+    var results = ctrl.results() ? ctrl.results() : [];
+    var player = ctrl.results() ? m.component(Player) : null;
+
+    return m('div', [
+      m.component(SearchForm),
+      m('div.player', [
+        m('div.container', [
+          m('div.responsive-embed', [
+            player
+          ])
+        ])
+      ]),
+      m('div.resultsContainer', [
+        results.filter(resultsWithImages).map((result, i) => {
+          return m.component(Result, {
+            name: result.name,
+            image: result.image[2]['#text'],
+            match: result.match,
+            index: i,
+          });
+        })
+      ])
+    ]);
+  }
+}
+
 var Result = {
   controller: function(args) {
+    this.index = args.index;
     this.name = args.name;
     this.image = args.image;
     this.match = args.match;
     this.tracks = [];
 
-    this.getTracks = function() {
+    this.getTracks = function(fn) {
       m.request({
         method: 'POST',
         url: '/tracks',
         data: { artist: this.name }
       }).then(body => {
         this.tracks = body.toptracks.track;
-      }).then(m.redraw)
+      }).then(fn)
     }.bind(this);
 
     this.youtubeFor = function(event) {
@@ -74,12 +95,22 @@ var Result = {
       }).then(m.redraw);
     }.bind(this);
 
+    this.initializePlayer = function(track) {
+      m.request({
+        method: 'POST',
+        url: '/video',
+        data: {artist: this.name, track: track}
+      }).then(body => {
+        this.updatePlayer(body.items[0].id.videoId);
+      });
+    }.bind(this);
+
     this.updatePlayer = function(id) {
       var node = document.getElementById('player');
 
       if (node.tagName === 'DIV') {
         window.player = new YT.Player('player', {
-          videoId: id
+          videoId: id,
         });
       } else {
         window.player.loadVideoById(id);
@@ -87,6 +118,12 @@ var Result = {
     }.bind(this);
   },
   view: function(ctrl) {
+    if (ctrl.index == 0 && ctrl.tracks.length < 1) {
+      ctrl.getTracks(() => {
+        ctrl.initializePlayer(ctrl.tracks[0].name);
+      });
+    }
+
     return m('div.result', [
       m('h3', ctrl.name),
       m('img', {src: ctrl.image}),
@@ -106,4 +143,9 @@ function resultsWithImages(result) {
   return result.image[2]['#text'];
 }
 
-m.mount(document.getElementById('testApp'), Search);
+m.route.mode = 'hash';
+m.route(document.getElementById('app'), '/', {
+  '/': SearchForm,
+  '/like/:artist': SearchResults,
+});
+// m.mount(document.getElementById('app'), Search);
